4fbd877850524d4a191a26a617c5a02b3f65f693
Sergii <http.serg.com@ukr.net>
2021-08-05 12:44:56 +0300

    Add Service

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿using Blog.ClassRepository;
+using Blog.Repository;
+using System;
 
 namespace Blog
 {
     class Program
     {
+        static EFContext context = new EFContext();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! ");
+            IPost postservice = new PostRepository(context);
+           
+            Service postserviceadd = new Service(postservice);
+
+            postserviceadd.AddPost(new ProducAddModel
+            {
+                Title = "Заголовок №1",
+                ShortDescription = "Короткий опим №1",
+                Description = "Максимально повний опис №1",
+                Meta = "Стиль №1",
+                UrlSlug = "www.blog/1",
+            });
+            postserviceadd.AddPost(new ProducAddModel
+            {
+                Title = "Заголовок №2",
+                ShortDescription = "Короткий опим №2",
+                Description = "Максимально повний опис №2",
+                Meta = "Стиль №1",
+                UrlSlug = "www.blog/2",
+            });
         }
     }
 }
--- Blog/Service.cs
+++ Blog/Service.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Blog.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,33 @@ using System.Threading.Tasks;
 
 namespace Blog
 {
+    public class ProducAddModel
+    {
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string Description { get; set; }
+        public string Meta { get; set; }
+        public string UrlSlug { get; set; }
+    }
+   
     public class Service
     {
+        private readonly IPost _postservice;
+        public Service (IPost postservice)
+        {
+            _postservice = postservice;
+        }
+        public void AddPost(ProducAddModel model)
+        {
+            var post = new Post();
+            post.Title = model.Title;
+            post.ShortDescription = model.ShortDescription;
+            post.Description = model.Description;
+            post.Meta = model.Meta;
+            post.UrlSlug = model.UrlSlug;
+            _postservice.Add(post);
+
+        }
+
     }
 }
